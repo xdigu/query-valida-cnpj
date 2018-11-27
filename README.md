30d9c78b1883a396364d552a49192c809aa3b805
Rodrigo jorge <xdigu@live.com>
2018-11-27 18:47:22 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# query-valida-cnpj" 
---
+++ js/content.js
@@ -0,0 +1,21 @@
+var titulo = document.getElementById('data-result').querySelectorAll('table > thead')
+var descricao = document.getElementById('data-result').querySelectorAll('table > tbody')
+var nomeFantasia
+var nomeEmpresa
+var cnpj
+var logradouro
+var numero
+var bairro
+var cidade
+var uf
+var cep
+var telefone
+
+for (valores in titulo){
+    if (titulo[valores] == ''){
+
+    }
+}
+
+
+
