5063e06b54248c7549350955884d598bec41199c
Rodrigo Jorge <xdigu@live.com>
2018-12-01 10:53:24 -0200

    Adaciona o resultado ao compo de texto

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,7 @@ var resposta
 document.addEventListener('DOMContentLoaded', function () {
     var btn = document.getElementById('carrega-informacao')
     btn.addEventListener('click', function () {
-    chrome.extension.sendMessage({ action: "sentFromPopUpToBackGround" });
+        chrome.extension.sendMessage({ action: "sentFromPopUpToBackGround" });
 
     })
 })
@@ -12,19 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action == 'sentFromBackGroundToPopUp') {
         resposta = request
-        setValores(resposta)
+            (function (resposta) {
+                document.getElementById('nome-empresa').value = resposta.nomeEmpresa
+                document.getElementById('nome-fantasia').value = resposta.nomeFantasia
+                document.getElementById('cnpj').value = resposta.cnpj
+                document.getElementById('logradouro').value = resposta.logradouro
+                document.getElementById('numero').value = resposta.numero
+                document.getElementById('bairro').value = resposta.bairro
+                document.getElementById('cidade').value = resposta.municipio
+                document.getElementById('uf').value = resposta.uf
+                document.getElementById('cep').value = resposta.cep
+                document.getElementById('telefone').value = resposta.telefone
+            })()
     }
 })
 
-function setValores(resposta) {
-    document.getElementById('nome-empresa').value = resposta.nomeEmpresa
-    document.getElementById('nome-fantasia').value = resposta.nomeFantasia
-    document.getElementById('cnpj').value = resposta.cnpj
-    document.getElementById('logradouro').value = resposta.logradouro
-    document.getElementById('numero').value = resposta.numero
-    document.getElementById('bairro').value = resposta.bairro
-    document.getElementById('cidade').value = resposta.municipio
-    document.getElementById('uf').value = resposta.uf
-    document.getElementById('cep').value = resposta.cep
-    document.getElementById('telefone').value = resposta.telefone
-}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.getElementById('gera-query')
+    btn.addEventListener('click', function () {
+        document.getElementById('resultado-query').innerHTML = 'funcionou!'
+
+    })
+})
