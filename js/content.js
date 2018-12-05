2aeef96c14e2438229197b86e50eaeaa11c77303
Rodrigo Jorge <xdigu@live.com>
2018-12-04 23:57:43 -0200

    Refatorando código

--- js/content.js
+++ js/content.js
@@ -12,52 +12,52 @@ window.setInterval(function () {
     titulo = document.getElementById('data-result').querySelectorAll('table > thead')
     descricao = document.getElementById('data-result').querySelectorAll('table > tbody')
 
-
-    // Percorre a tabela verificando se possui as informações no titulo
+    // Percorre a tabela pelo titulo e descrição
     for (var percorre1 = 0; percorre1 < titulo.length; percorre1++) {
         for (var percorre2 = 0; percorre2 < titulo[percorre1].querySelectorAll('thead > tr > th').length; percorre2++) {
-            if (titulo[percorre1].querySelectorAll('thead > tr > th')[percorre2].innerHTML) {
-                
 
-                // Adiciona as informações da descrição da empresa para a resposta
+            // Verifica se possui alguma infomação no titulo
+            if (titulo[percorre1].querySelectorAll('thead > tr > th')[percorre2].innerHTML) {
+
+                // Adiciona as informações da descrição da empresa para o objeto resposta
                 switch (titulo[percorre1].querySelectorAll('thead > tr > th')[percorre2].innerHTML) {
                     case 'Nome Empresarial':
-                    resposta.nomeEmpresa = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    resposta.cnpj = document.getElementsByTagName('h5')[0].innerText.substr(5,18).replace(/[\./-]/g, "")
-                    break
+                        resposta.nomeEmpresa = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        resposta.cnpj = document.getElementsByTagName('h5')[0].innerText.substr(5, 18).replace(/[\./-]/g, "")
+                        break
                     case 'Nome Fantasia':
-                    resposta.nomeFantasia = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.nomeFantasia = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'Logradouro':
-                    resposta.logradouro = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.logradouro = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'Número':
-                    resposta.numero = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.numero = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'CEP':
-                    resposta.cep = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML.replace(/[\./-]/g, "")
-                    break
+                        resposta.cep = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML.replace(/[\./-]/g, "")
+                        break
                     case 'Bairro':
-                    resposta.bairro = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.bairro = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'Município':
-                    resposta.municipio = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.municipio = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'UF':
-                    resposta.uf = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.uf = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                     case 'Telefone':
-                    resposta.telefone = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
-                    break
+                        resposta.telefone = descricao[percorre1].querySelectorAll('tbody > tr > td')[percorre2].innerHTML
+                        break
                 }
             }
         }
     }
+
 
     // Envia resposta para o background
     if (resposta) {
         resposta.action = 'sentFromContentToBackGround'
         chrome.runtime.sendMessage(resposta)
     }
-
 }, 5000)
